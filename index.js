7a08fa1796092d0f843697fc581edb15b847f15f
RoneyVaLo <rvaldelomarlopez@gmail.com>
2024-01-04 15:24:43 -0600

    add cors rules and endpoint to game routes

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import { connectToDatabase } from './db.js';
+import gameRoutes from './src/routes/gameRoutes.js';
 
 const app = express();
 const PORT = 4000;
+
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
 
 app.use(express.json());
 
 connectToDatabase();
 
-app.get('/game', (req, res) => {
-    res.send("/game")
-});
+app.use("/api", gameRoutes);
 
 
 app.listen(PORT, () => {
